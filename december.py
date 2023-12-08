32b920485ec90fb2e33a14556a6992dbf77f9e1a
Monday_David_Sarka <davidsarka242@gmail.com>
2023-12-08 22:21:22 +0100

    Printed out rows in a loop

--- december.py
+++ december.py
@@ -14,4 +14,16 @@ world_titles = table.find_all('th')
 
 world_table_titles = [title.text.strip() for title in world_titles]
 
-print(world_table_titles)
+df = pd.DataFrame(columns= world_titles)
+
+column_data = table.find_all('tr')
+
+for row in column_data:
+  row_data = row.find_all('td')
+  individual_row_data = [data.text.strip() for data in row_data]
+  print(individual_row_data)
+  
+
+# print(df)
+# print(column_data)
+# print(individual_row_data)
